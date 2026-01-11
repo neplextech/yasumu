e9bb5c7a0df02717804519ee83f5a27d5101f2a0
twlite <46562212+twlite@users.noreply.github.com>
2026-01-11 16:25:58 +0545

    feat: expose workspace object to rest scripts

--- packages/common/src/types/emails/email-script.ts
+++ packages/common/src/types/emails/email-script.ts
@@ -1,26 +1,7 @@
 import type { CommonScriptRuntimeContext } from '../common/common.types.js';
 import type { EmailData } from './email.js';
 
-export interface EmailScriptWorkspaceContext {
-  /**
-   * The id of the workspace.
-   */
-  id: string;
-  /**
-   * The name of the workspace.
-   */
-  name: string;
-  /**
-   * The path of the workspace.
-   */
-  path: string | null;
-}
-
 export interface EmailScriptContext extends CommonScriptRuntimeContext {
-  /**
-   * The workspace context.
-   */
-  workspace: EmailScriptWorkspaceContext;
   /**
    * The email entity
    */
